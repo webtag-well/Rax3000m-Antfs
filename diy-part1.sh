cb17cb4f7c9836540b17ad7896614ac55d865e56
webtag-well <50027762+webtag-well@users.noreply.github.com>
2025-12-12 15:59:52 +0800

    Uncomment feed source and add new feeds

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,6 +14,6 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git oaf https://github.com/destan19/OpenAppFilter.git' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-#git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+echo 'src-git oaf https://github.com/destan19/OpenAppFilter.git' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
